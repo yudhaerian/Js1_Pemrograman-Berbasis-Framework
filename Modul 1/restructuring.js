2d2cea4c161f291ee1d2b1eef7fdad1c31c238f0
yudhaerian <55540987+yudhaerian@users.noreply.github.com>
2022-02-16 10:29:58 +0700

    Modul 1

---
+++ Modul 1/restructuring.js
@@ -0,0 +1,8 @@
+var PemainSepakbola = {
+	name: 'Salah',
+	height: '175',
+	output(){
+		alert(`Mr. ${this.name} is  ${this.height} centimeter tall`);
+	}
+};
+PemainSepakbola.output();
---
+++ Modul 1/template.js
@@ -0,0 +1,6 @@
+let fname = 'Cristian';
+let Iname = 'Ronaldo';
+let age = prompt ("Masukkan umur Cristian Ronaldo!");
+
+let result = `${fname} ${Iname} is ${age} years old`;
+alert(result);
