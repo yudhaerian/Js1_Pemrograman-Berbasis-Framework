9f0376dab4717edfabe4be843688a8b60dbc724a
yudhaerian <55540987+yudhaerian@users.noreply.github.com>
2022-04-26 13:07:00 +0700

    Modul 10

---
+++ Modul 10/firebase/src/Join.js
@@ -0,0 +1,72 @@
+import React, { useState, useContext } from 'react';
+import { AuthContext } from './index';
+import firebase from 'firebase';
+
+const Join = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setErrors] = useState("");
+
+    const Auth = useContext(AuthContext);
+    const handleForm = e => {
+        e.preventDefault();
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(email, password)
+            .then(res => {
+                if (res.user) Auth.setLoggedIn(true);
+            })
+            .catch (e => {
+                setErrors(e.message);
+            });
+    };
+
+    const handleLogin = () => {
+        const base_provider = new firebase.auth.GoogleAuthProvider();
+        firebase.
+            auth().
+            signInWithPopup(base_provider)
+            .then(res => {
+                Auth.setLoggedIn(true);
+                alert("Join Successfully");
+            })
+            .catch(e => {
+                setErrors(e.message);
+            });
+    };
+
+
+    return (
+        <div>
+            <h1> Join </h1>
+            <form onSubmit= {e => handleForm(e)}>
+                <input 
+                    value = {email}
+                    onChange = {e => setEmail(e.target.value)}
+                    name = "email"
+                    type = "email"
+                    placeholder = "email"
+                />
+                <input 
+                    onChange = {e => setPassword(e.target.value)}
+                    name = "password"
+                    value = {password}
+                    type = "password"
+                    placeholder = "password"
+                />
+                <hr />
+                <button class="googleBtn" type="button" onClick={handleLogin}>
+                    <img
+                        src = "https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                        alt = "logo"
+                    />
+                    Join With Google
+                </button>
+                <button type="submit"> Join </button>
+                <span>{error}</span>
+            </form>
+        </div>
+    );
+};
+
+export default Join;
---
+++ Modul 10/firebase/src/firebase.config.js
@@ -0,0 +1,10 @@
+export default {
+     apiKey: "AIzaSyAnSXStc4ysEm1X6ETDkx_xi1o1w74SiNM",
+     authDomain: "yudhaerian.firebaseapp.com",
+     projectId: "yudhaerian",
+     storageBucket: "yudhaerian.appspot.com",
+     messagingSenderId: "650687867117",
+     appId: "1:650687867117:web:ca9a60986eb334c54684e4",
+     measurementId: "G-91C9NTHQL4"
+};
+
